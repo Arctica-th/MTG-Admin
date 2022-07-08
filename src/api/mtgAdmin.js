ea675ebf61fb19ab570277bee0f73573173333c6
Peeradis <die9994@hotmail.com>
2022-07-08 11:40:56 +0000

    merged

--- src/api/mtgAdmin.js
+++ src/api/mtgAdmin.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const mtgApi = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://dd-api-service.destinydraw",
   headers: {
     "Content-Type": "application/json",
   },
