5aa7b43d66284da49ccf30d8ae5fe89dbfc9774b
Narut Saharn <narut.sh@gmail.com>
2024-08-07 20:03:32 +0700

    update dockerfile

--- src/Layout/MainLayout.js
+++ src/Layout/MainLayout.js
@@ -35,7 +35,7 @@ const MainLayout = () => {
         </div>
 
         <div className="position-absolute bottom-0 end-0 p-2 text-secondary">
-          version 0.0.16
+          version 0.0.17
         </div>
       </div>
     </React.Fragment>
