0dcf92ce7a08488c9c27d47ba83ee988f3b7310e
Narut Saharn <narut.sh@gmail.com>
2024-08-28 17:02:18 +0700

    update version

--- src/Layout/MainLayout.js
+++ src/Layout/MainLayout.js
@@ -37,7 +37,7 @@ const MainLayout = () => {
         </div>
 
         <div className="position-absolute bottom-0 end-0 p-2 text-secondary">
-          version 0.0.22
+          version 0.0.23
         </div>
       </div>
     </React.Fragment>
